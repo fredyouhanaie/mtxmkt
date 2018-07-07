dd2b4afba807901f0db93e4ad5b2d6311358d689
Fred Youhanaie <fyrlang@anydata.co.uk>
2018-07-07 18:27:53 +0100

    added function to read and check the banner line.
    Also included are a set of test files and eunit tests for the banner
    line tests.

--- src/mtxmkt.erl
+++ src/mtxmkt.erl
@@ -1,7 +1,17 @@
 -module(mtxmkt).
 
 %% API exports
--export([mm_openread/1]).
+-export([mm_openread/1, mm_read_banner/1]).
+
+%%====================================================================
+%% Macros
+%%====================================================================
+
+-define(Blanks, " \t").
+-define(MtxType, [array, coordinate]).
+-define(DataType, [real, complex, pattern, integer]).
+-define(SymmType, [general, symmetric, hermitian, 'skew-symmetric']).
+-define(MatrixMarketBanner, "%%MatrixMarket ").
 
 
 %%====================================================================
@@ -10,11 +20,13 @@
 
 
 %%--------------------------------------------------------------------
-%% @doc open a file for reading
+%% @doc Open a matrix file for reading.
+%%
 %% @spec
+%% mm_openread(filename()) -> io_device() | {error, atom(), string()}
 %% @end
 %%--------------------------------------------------------------------
-
+-spec mm_openread(string()) -> pid() | {error, atom(), string()}.
 mm_openread(Filename) ->
     case file:open(Filename, [read]) of
 	{ok, IOdev} ->
@@ -23,7 +35,140 @@ mm_openread(Filename) ->
 	    {error, Reason, "Could not open the file"}
     end.
 
+%%--------------------------------------------------------------------
+%% @doc Read and process the matrix file header/banner
+%%
+%% For this to succeed the file pointer should be at the very start of
+%% the file.
+%%
+%% If successful, returns {ok, Banner}, where Banner is a quadruple of
+%% the fields of the banner line.
+%%
+%% @spec
+%% mm_read_banner(io_device()) -> {atom(), atom(), atom(), atom()} | {error, atom(), string()}
+%% @end
+%% --------------------------------------------------------------------
+-spec mm_read_banner(pid()) -> {atom(), atom(), atom(), atom()} | {error, atom(), string()}.
+mm_read_banner(IOdev) ->
+    case file:read_line(IOdev) of
+	{ok, Banner} ->
+	    process_banner(Banner);
+	eof ->
+	    {error, eof, "Premature end of file"};
+	{error, Reason} ->
+	    {error, Reason, "Could not read from file"}
+    end.
+
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+
+%%--------------------------------------------------------------------
+%% @doc Check and process the banner line.
+%%
+%% @spec
+%% process_banner(string()) -> {atom(), atom(), atom(), atom()} | {error, atom(), string()}
+%% @end
+%%--------------------------------------------------------------------
+-spec process_banner(string()) -> {atom(), atom(), atom(), atom()} | {error, atom(), string()}.
+process_banner(Banner) ->
+    Rest = string:prefix(Banner, ?MatrixMarketBanner),
+    case Rest of
+	nomatch ->
+	    {error, mm_no_header, "Not a valid Matrix Market file"};
+	_ ->
+	    Fields = str2atoms(string:trim(Rest)),
+	    case length(Fields) of
+		4 ->
+		    process_banner_rest(Fields);
+		_ ->
+		    {error, mm_invalid_header, "Invalid banner line"}
+	    end
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Check the quadruple fields on the banner line.
+%%
+%% @spec
+%% process_banner_rest(list()) -> {atom(), atom(), atom(), atom()} | {error, tuple(), string()}
+%% @end
+%%--------------------------------------------------------------------
+-spec process_banner_rest(list()) -> {atom(), atom(), atom(), atom()} | {error, tuple(), string()}.
+process_banner_rest([Obj, Fmt, Type, Symm]) ->
+    case Obj of
+	matrix ->
+	    case check_banner_mtxtype(Fmt, Type, Symm) of
+		ok ->
+		    {Obj, Fmt, Type, Symm};
+		Error ->
+		    Error
+	    end;
+	_ ->
+	    {error, mm_unsupported_type, "Unsupported object type, must be matrix"}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Check the second field, format, of the banner.
+%%
+%% @spec
+%% check_banner_mtxtype(atom(), atom(), atom()) -> ok | {error, atom(), string()}
+%% @end
+%%--------------------------------------------------------------------
+-spec check_banner_mtxtype(atom(), atom(), atom()) -> ok | {error, atom(), string()}.
+check_banner_mtxtype(Fmt, Type, Symm) ->
+    case lists:member(Fmt, ?MtxType) of
+	true ->
+	    check_banner_datatype(Type, Symm);
+	_ ->
+	    {error, mm_unsupported_type, "Unsupported matrix type, not array/coordinate"}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Check the third field, data type, of the banner.
+%%
+%% @spec
+%% check_banner_datatype(atom(), atom()) -> ok | {error, atom(), string()}
+%% @end
+%%--------------------------------------------------------------------
+-spec check_banner_datatype(atom(), atom()) -> ok | {error, atom(), string()}.
+check_banner_datatype(Type, Symm) ->
+    case lists:member(Type, ?DataType) of
+	true ->
+	    check_banner_symmetry(Symm);
+	_ ->
+	    {error, mm_unsupported_type, "Unsupported matrix data type"}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Check the fourth field, symmetry, of the banner line.
+%%
+%% @spec
+%% check_banner_symmetry(atom()) -> ok | {error, atom(), string()}
+%% @end
+%%--------------------------------------------------------------------
+-spec check_banner_symmetry(atom()) -> ok | {error, atom(), string()}.
+check_banner_symmetry(Symm) ->
+    case lists:member(Symm, ?SymmType) of
+	true ->
+	    ok;
+	_ ->
+	    {error, mm_unsupported_type, "Unsupported matrix symmetry type"}
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Return the blank separated tokens in a string as a list of atoms.
+%%
+%% @spec
+%% str2atoms(string()) -> list()
+%% @end
+%%--------------------------------------------------------------------
+-spec str2atoms(string()) -> list().
+str2atoms (Line) ->
+    StrList = string:lexemes(Line, ?Blanks),
+    lists:map(
+      fun (S) ->
+	      erlang:list_to_atom(string:lowercase(S))
+      end,
+      StrList).
--- test/mtxmkt_tests.erl
+++ test/mtxmkt_tests.erl
@@ -12,6 +12,14 @@
 
 -define(File_nofile, "test/nofile.mtx"). %% this file should not exist!
 -define(File_nulldev, "/dev/null"). %% will work in Unix, not sure about windows!
+-define(File_non_mm, "test/testfile-non-mm.mtx").
+-define(File_short_banner, "test/testfile-short-banner.mtx").
+-define(File_extra_banner, "test/testfile-extra-banner.mtx").
+-define(File_invalid_object, "test/testfile-invalid-object.mtx").
+-define(File_invalid_format, "test/testfile-invalid-format.mtx").
+-define(File_invalid_datatype, "test/testfile-invalid-datatype.mtx").
+-define(File_invalid_symmetry, "test/testfile-invalid-symmetry.mtx").
+-define(File_valid_banner, "test/testfile-valid-banner.mtx").
 
 %%--------------------------------------------------------------------
 %% The tests
@@ -25,4 +33,67 @@ nofile_open_test() ->
 emptyfile_open_test() ->
     IOdev = mtxmkt:mm_openread(?File_nulldev),
     ?assert(is_pid(IOdev)),
+    file:close(IOdev).
+
+% empty file has no banner
+emptyfile_banner_test() ->
+    IOdev = mtxmkt:mm_openread(?File_nulldev),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, eof, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% Not a matrix market file
+nonmmfile_test() ->
+    IOdev = mtxmkt:mm_openread(?File_non_mm),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_no_header, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% missing banner fields
+banner_missing_fields_test() ->
+    IOdev = mtxmkt:mm_openread(?File_short_banner),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_invalid_header, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% too many banner fields
+banner_extra_fields_test() ->
+    IOdev = mtxmkt:mm_openread(?File_extra_banner),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_invalid_header, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% invalid banner field - object
+banner_invalid_object_test() ->
+    IOdev = mtxmkt:mm_openread(?File_invalid_object),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_unsupported_type, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% invalid banner field - format
+banner_invalid_format_test() ->
+    IOdev = mtxmkt:mm_openread(?File_invalid_format),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_unsupported_type, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% invalid banner field - data type
+banner_invalid_datatype_test() ->
+    IOdev = mtxmkt:mm_openread(?File_invalid_datatype),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_unsupported_type, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% invalid banner field - symmetry
+banner_invalid_symmetry_test() ->
+    IOdev = mtxmkt:mm_openread(?File_invalid_symmetry),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({error, mm_unsupported_type, _Msg}, mtxmkt:mm_read_banner(IOdev)),
+    file:close(IOdev).
+
+% valid banner
+banner_valid_banner_test() ->
+    IOdev = mtxmkt:mm_openread(?File_valid_banner),
+    ?assert(is_pid(IOdev)),
+    ?assertMatch({matrix, array, real, general}, mtxmkt:mm_read_banner(IOdev)),
     file:close(IOdev).
